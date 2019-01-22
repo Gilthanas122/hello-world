bf985996870ce14988e5399c49ac83d9efb1e592
Gilthanas122 <pityu.django@gmail.com>
2019-01-22 16:57:37 +0100

    path to .jar file corrected

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 ps | grep hello-world | awk '{print $1}' | xargs kill -9 || true
-env SERVER.PORT=8081 nohup java -jar ./target/hello-world-1.0-SNAPSHOT.jar &
+env SERVER.PORT=8081 nohup java -jar ./build/libs/demohelloworld-0.0.1-SNAPSHOT.jar &
