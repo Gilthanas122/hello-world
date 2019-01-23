915942f29f72be9e20c45071c0435cf7fd570a3b
Gilthanas122 <pityu.django@gmail.com>
2019-01-23 14:45:27 +0100

    again jenkins

--- src/main/java/com/helloworld/demohelloworld/HomeController.java
+++ src/main/java/com/helloworld/demohelloworld/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HomeController {
 
     @RequestMapping("/")
-    public @ResponseBody String greetingxx() {
+    public @ResponseBody String greetingxdsx() {
         return "Hello World2";
     }
 
