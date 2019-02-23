b64b63e224c72282790aec5c55485d80cac0ce2c
Gilthanas122 <pityu.django@gmail.com>
2019-02-23 17:43:19 +0100

    sh remade

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,19 @@
-ps | grep hello-world | awk '{print $1}' | xargs kill -9 || true
-env SERVER.PORT=8081 nohup java -jar ./build/libs/demohelloworld-0.0.1-SNAPSHOT.jar &
+#!/usr/bin/env bash
+gradle build
+echo "pass in your username"
+read DOCKER_USERNAME
+echo "type in your password for dockerhub"
+read DOCKER_PASSWORD
+echo "type the repository on dockerhub"
+read DOCKER_REPOSITORY
+DOCKER_IMAGE=$((1 + $RANDOM % 1000))
+DOCKER_CONTAINER=$((1 + $RANDOM % 1000))
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+docker build -t $DOCKER_IMAGE .
+docker tag $DOCKER_IMAGE $DOCKER_USERNAME/$DOCKER_REPOSITORY
+docker push $DOCKER_USERNAME/$DOCKER_REPOSITORY
+docker run --name $DOCKER_CONTAINER -d -it $DOCKER_IMAGE -P 8080
+sleep 10
+docker stop $DOCKER_CONTAINER
+docker rm $DOCKER_CONTAINER
+docker rmi -f $DOCKER_IMAGE
